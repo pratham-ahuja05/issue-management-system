c45f042cdd07d69319aac084e7af9b809af6973f
Pratham Ahuja <ahujapa05@gmail.com>
2026-07-26 01:32:36 +0530

    Add Role model for authorization

---
+++ backend/issue-management-backend/issue-management/src/main/java/com/issue_management/model/Role.java
@@ -0,0 +1,39 @@
+package com.issue_management.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(unique = true, nullable = false)
+    private RoleType name;
+
+    public Role() {}
+
+    public Role(RoleType name) {
+        this.name = name;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public RoleType getName() {
+        return name;
+    }
+
+    public void setName(RoleType name) {
+        this.name = name;
+    }
+}
